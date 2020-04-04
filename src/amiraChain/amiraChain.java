561acff5411b6aa090d621204d7257af7302f615
strauss7 <wbsabbyyuji@gmail.com>
2020-04-04 16:06:10 -0300

    Updating amiraChain class

--- src/amiraChain/amiraChain.java
+++ src/amiraChain/amiraChain.java
@@ -7,14 +7,26 @@ import com.google.gson.GsonBuilder;
 public class amiraChain {
 	
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
+	public static int difficulty = 5;
 
 	public static void main(String[] args) {
 		
 		blockchain.add(new Block("Hi im the first block", "0"));
+		System.out.println("Trying to Mine block 1...");
+		blockchain.get(0).mineBlock(difficulty);
+		
 		blockchain.add(new Block("Hi im the second block", blockchain.get(blockchain.size()-1).hash));
+		System.out.println("Trying to Mine block 2...");
+		blockchain.get(1).mineBlock(difficulty);
+		
 		blockchain.add(new Block("Hi im the third block", blockchain.get(blockchain.size()-1).hash));
+		System.out.println("Trying to Mine block 3...");
+		blockchain.get(2).mineBlock(difficulty);
+		
+		System.out.println("\nBlockchain is Valid: " + isChainValid());
 		
 		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+		System.out.println("\nThe block chain: ");
 		System.out.println(blockchainJson);
 	}
 	
