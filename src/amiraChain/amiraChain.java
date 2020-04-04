b8e6c14b56998d786207172caa7b4976a39c694d
strauss7 <wbsabbyyuji@gmail.com>
2020-04-04 15:39:03 -0300

    Testing

---
+++ src/amiraChain/amiraChain.java
@@ -0,0 +1,18 @@
+package amiraChain;
+
+public class amiraChain {
+
+	public static void main(String[] args) {
+		
+		Block genesisBlock = new Block("Hi im the first block", "0");
+		System.out.println("Hash for block 1: " + genesisBlock.hash);
+		
+		Block secondBlock = new Block("Hi im the second block", genesisBlock.hash);
+		System.out.println("Hash for block 2: " + secondBlock.hash);
+		
+		Block thirdBlock = new Block("Hi im the third block", secondBlock.hash);
+		System.out.println("Hash for block 3: " + thirdBlock.hash);
+		
+	}
+
+}
